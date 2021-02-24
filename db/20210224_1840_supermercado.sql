b1a8c92b70945c8c3c7c0ea3fa1d747bb4ef8dba
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-02-24 18:41:08 +0100

    Añadida basse de datos actualizada

--- db/20210224_1840_supermercado.sql
+++ db/20210224_1840_supermercado.sql
@@ -18,40 +18,37 @@ CREATE DATABASE IF NOT EXISTS `supermercado` /*!40100 DEFAULT CHARACTER SET utf1
 USE `supermercado`;
 
 -- Volcando estructura para tabla supermercado.compras
-DROP TABLE IF EXISTS `compras`;
 CREATE TABLE IF NOT EXISTS `compras` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `empleado_id` int(11) NOT NULL,
-  `fecha` datetime NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `FK_compras_empleados` (`empleado_id`),
   CONSTRAINT `FK_compras_empleados` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf16 COLLATE=utf16_spanish_ci;
 
--- Volcando datos para la tabla supermercado.compras: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla supermercado.compras: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `compras` DISABLE KEYS */;
 /*!40000 ALTER TABLE `compras` ENABLE KEYS */;
 
 -- Volcando estructura para tabla supermercado.detalles
-DROP TABLE IF EXISTS `detalles`;
 CREATE TABLE IF NOT EXISTS `detalles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cantidad` smallint(5) unsigned NOT NULL,
-  `producto_id` int(11) NOT NULL,
   `compra_id` int(11) DEFAULT NULL,
+  `producto_id` int(11) NOT NULL,
+  `cantidad` smallint(5) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_detalles_productos` (`producto_id`),
   KEY `FK_detalles_compras` (`compra_id`),
   CONSTRAINT `FK_detalles_compras` FOREIGN KEY (`compra_id`) REFERENCES `compras` (`id`),
   CONSTRAINT `FK_detalles_productos` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf16 COLLATE=utf16_spanish_ci;
 
--- Volcando datos para la tabla supermercado.detalles: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla supermercado.detalles: ~7 rows (aproximadamente)
 /*!40000 ALTER TABLE `detalles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `detalles` ENABLE KEYS */;
 
 -- Volcando estructura para tabla supermercado.empleados
-DROP TABLE IF EXISTS `empleados`;
 CREATE TABLE IF NOT EXISTS `empleados` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) COLLATE utf16_spanish_ci NOT NULL,
@@ -59,14 +56,15 @@ CREATE TABLE IF NOT EXISTS `empleados` (
   `ultima_sesion` datetime DEFAULT NULL,
   `fecha_contratacion` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf16 COLLATE=utf16_spanish_ci;
 
 -- Volcando datos para la tabla supermercado.empleados: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` (`id`, `nombre`, `apellidos`, `ultima_sesion`, `fecha_contratacion`) VALUES
+	(1, 'Empleado1', 'Apellidos', '2021-02-24 17:33:01', '2017-02-17 19:34:48');
 /*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
 
 -- Volcando estructura para tabla supermercado.productos
-DROP TABLE IF EXISTS `productos`;
 CREATE TABLE IF NOT EXISTS `productos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) COLLATE utf16_spanish_ci DEFAULT NULL,
@@ -80,9 +78,9 @@ CREATE TABLE IF NOT EXISTS `productos` (
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
 INSERT INTO `productos` (`id`, `nombre`, `precio_venta`, `precio_proveedor`, `cantidad_stock`) VALUES
 	(1, 'Disco duro', 8999, 7000, 120),
-	(2, 'Teclado', 5000, 4000, 60),
-	(3, 'Micrófono', 1999, 1500, 47),
-	(4, 'Ratón', 1195, 800, 32);
+	(2, 'Teclado', 5000, 4000, 50),
+	(3, 'Micrófono', 1999, 1500, 41),
+	(4, 'Ratón', 1195, 800, 50);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
